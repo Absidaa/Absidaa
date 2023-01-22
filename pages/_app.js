dd6dadd7d4aeabdcac9b1536711b604a18348e7c
Your Name <absida8@gmail.com>
2023-01-22 16:09:07 +0100

    bug fixed

--- pages/_app.js
+++ pages/_app.js
@@ -39,8 +39,15 @@ export default function App({ Component, pageProps }) {
           rel="stylesheet"
           href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"
         />
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+        <Link
+          rel="stylesheet"
+          href="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"
+        />
+
+        <Link
+          rel="stylesheet"
+          href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"
+        />
       </div>
     </>
   );
